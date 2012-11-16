21e3e16e66d253c18d98a1ae6d5f6098ae59c5c6
Bob Larrick <larrick@gmail.com>
2012-11-16 16:56:30 -0500

    savepoint

--- passenger.rb
+++ passenger.rb
@@ -1,6 +1,13 @@
 
 class Passenger
-  attr_accessor :raw_attrs, :arr, :survived, :pclass, :name, :sex, :age, :sibsp, :parch, :ticket, :fare, :cabin, :embarked
+
+  @@user_distances = {}
+  @@count = 0
+
+
+
+  attr_accessor :raw_attrs, :arr, :survived, :pclass, :name, :sex, :age, :sibsp, :parch, :ticket, :fare, :cabin, :embarked, :predicted
+  attr_accessor :my_key
 
   def initialize(arr)
     @arr = arr
@@ -8,6 +15,19 @@ class Passenger
     @raw_attrs.each do |k, v|
       send("#{k}=", v)
     end
+    @answers = {}
+  end
+
+  def pclass=(val)
+    @pclass = val.to_i
+  end
+
+  def sibsp=(val)
+    @sibsp = val.to_i
+  end
+
+  def parch=(val)
+    @parch = val.to_i
   end
 
   def headers
@@ -18,29 +38,60 @@ class Passenger
     survived == '1'
   end
 
+  def fare=(val)
+    return -1 unless val
+    @fare = case val.to_i
+            when 0..10
+              1
+            when 10..20
+              2
+            when 20..30
+              3
+            when 30..40
+              4
+            when 40..50
+              5
+            when 50..60
+              6
+            when 60..70
+              7
+            when 70..80
+              8
+            when 80..90
+              9
+            when 90..100
+              10
+            when 100..200
+              12
+            when 200..1000
+              15
+            end
+  end
+
+
   def age=(val)
     @age = case val.to_i
            when 0..10
              0
-           when 11..20
+           when 10..20
              1
-           when 21..30
+           when 20..30
              2
-           when 31..40
+           when 30..40
              3
-           when 41..50
+           when 40..50
              4
-           when 51..60
+           when 50..60
              5
-           when 61..70
+           when 60..70
              6
-           when 71..80
+           when 70..80
              7
-           when 81..90
+           when 80..90
              8
            when 90..100
              9
-           when 101..1000
+           when 100..1000
              10
            else
              -1
@@ -69,12 +120,49 @@ class Passenger
   end
 
   def cabin_letter
-    @raw_attrs[:cabin].match(/\D/)[0]
+    @cabin_letter ||= if @raw_attrs['cabin']
+            foo = @raw_attrs['cabin'].match(/\D/)
+            foo[0] if foo
+          end
   end
 
   def cabin_number
-    @raw_attrs[:cabin].match(/\d+/)[0]
+    return @cabin_number if @cabin_number
+    bar = if @raw_attrs['cabin']
+            foo = @raw_attrs['cabin'].match(/\d+/)
+            foo[0] if foo
+          end
+
+    return @cabin_number = -1 unless bar
+    @cabin_number = case bar.to_i
+    when 0..10
+      0
+    when 10..20
+      1
+    when 20..30
+      2
+    when 30..40
+      3
+    when 40..50
+      4
+    when 50..60
+      5
+    when 60..70
+      6
+    when 70..80
+      7
+    when 80..90
+      8
+    when 90..100
+      9
+    when 100..1000
+      10
+    else
+      -1
+    end
+
   end
+
 
   def sex=(val)
     @sex = case val
@@ -101,8 +189,47 @@ class Passenger
   end
 
   def gimme(ar)
-    ar.map do |k|
-      send(k)
+    return @answers[ar] if @answers[ar]
+    puts 'should not hit this'
+
+    @answers[ar] = ar.map{|k| send(k) }
+  end
+
+  def precompute_gimme(ar)
+    (2..ar.count).each do |int|
+      ar.combination(int).to_a.each do |foo|
+        @answers[foo] = foo.map{|x| send(x) }
+      end
+    end
+  end
+
+  def distance_to(user, arr)
+    mine = gimme(arr)
+    theirs = user.gimme(arr)
+    key = mine.join.to_i
+    t_k = theirs.join.to_i
+
+    if @@user_distances[arr].nil?
+      @@user_distances[arr] = []
+    end
+    if @@user_distances[arr][key].nil?
+      @@user_distances[arr][key] = []
+    end
+    if @@user_distances[arr][key][t_k]
+      return @@user_distances[arr][key][t_k]
+    end
+
+    sum_of_squares = mine.zip(theirs).map{|x| x[0] - x[1]}.map{|x| x ** 2}.reduce(:+)
+    ans = Math.sqrt(sum_of_squares)
+
+    @@user_distances[arr][key][t_k] = ans
+  end
+
+  def predicted=(val)
+    if [true, '1', 1].include?(val)
+      @predicted = 1
+    else
+      @predicted = 0
     end
   end
 
--- ruby1.rb
+++ ruby1.rb
@@ -1,22 +1,95 @@
 require './passenger'
 require 'csv'
+require 'gruff'
+require 'debugger'
 
 foo = CSV.read('train.csv')
 headers = foo.shift
 
 bar = foo.map{|x| Passenger.new(x)}
+live, dead = bar.partition{|x| x.survived?}
 
-
-headers = ["pclass", "name", "sex", "age", "sibsp", "parch", "ticket", "fare", "cabin", "embarked"].map(&:intern)
-headers = ["pclass", "sex", "age", 'sibsp', 'parch', "embarked", "cabin"].map(&:intern)
+headers = ["embarked", "sex", "pclass", "age", 'sibsp', 'parch', "cabin", "fare", "cabin_number"].map(&:intern)
 
 puts 'total passengers'
 puts total_passengers = bar.count
 
+bar.each{|x| x.precompute_gimme(headers) }
+
+puts bar.map{|x| x.cabin_number}.uniq.compact.inspect
+
+bats = headers.combination(2).to_a
+
+puts "live " + live.count.to_s
+puts "dead " + dead.count.to_s
+
+k = 3
+n = 100
+s = headers.count
+
+best = [0, nil]
+
+if profile = false
+  require 'ruby-prof'
+end
+
+(2..3).each do |num|
+  headers.combination(num).each do |heads|
+    puts heads.inspect
+
+    if profile
+      RubyProf.start
+    end
+    bar.each_with_index do |user, idx|
+      tommy = bar.sort_by{|x| user.distance_to(x, heads)}.take(k)
+      user.predicted = (tommy.reject{|x| x == user}.map{|x| x.survived? == true ? 1 : 0}.reduce(:+) / k.to_f).round
+    end
+
+    right, wrong = bar.partition{|x| x.predicted.to_s == x.survived}
 
 
-bats = headers.permutation(2).to_a
+    puts 'right ' + right.count.to_s
+    puts 'wrong ' + wrong.count.to_s
+    percentage = (right.count / bar.count.to_f)
+    puts 'percentage ' + percentage.to_s
+    if best[0] < percentage
+      best[0] = percentage
+      best[1] = heads
+      puts ' ---------------------------- ' + heads.join(', ') + ' takes the lead!'
+    end
 
+    if profile
+      prof_result = RubyProf.stop
+      printer = RubyProf::FlatPrinter.new(prof_result)
+      printer.print(STDOUT)
+    end
+    raise 'round 1'
+  end
+end
+
+
+# # gruff stuff
+# bats.each do |bat|
+#   puts "by " << bat.join(", ")
+
+#   g_live = Gruff::Scatter.new(800)
+#   g_live.title = bat.join(", ") << ' live'
+#   g_live.data(:control, [0, 3], [0, 3])
+#   live_xs = live.map{|x| x.send(:gimme, bat)[0].to_i }
+#   live_ys = live.map{|x| x.send(:gimme, bat)[1].to_i }
+#   g_live.data(:live, live_xs, live_ys)
+#   g_live.write('pngs/' + g_live.title + ".png")
+
+#   g_dead = Gruff::Scatter.new(800)
+#   g_dead.title = bat.join(", ") << ' dead'
+#   g_dead.data(:control, [0, 3], [0, 3])
+#   dead_xs = dead.map{|x| x.send(:gimme, bat)[0].to_i }
+#   dead_ys = dead.map{|x| x.send(:gimme, bat)[1].to_i }
+#   g_dead.data(:dead, dead_xs, dead_ys)
+#   g_dead.write('pngs/' + g_dead.title + ".png")
+
+# end
+# __END__
 
 
 headers.each do |header|
@@ -32,20 +105,37 @@ headers.each do |header|
   end
 end
 
+#__END__
 
-
-puts '_______________'
+puts '__________________________________________________________________________________________________________________________________________________________'
 
 bats.each do |bat|
   puts "by " << bat.join(", ")
   values = bar.map{|x| x.gimme(bat)}.uniq
   values.each do |val|
+
     cow = bar.select{|x| x.send(:gimme, bat) == val}
+    live, dead = cow.partition{|x| x.survived?}
     unless cow.count < 10
-      puts "\t" << val.join(", ")
-      puts "\t\t" << cow.count.to_s <<  " people had #{val} for #{bat}"
-      puts "\t\t" << (cow.select{|x| x.survived?}.count / cow.count.to_f).round(3).to_s << " of those survived"
-      puts "\t\t" << cow.select{|x| x.survived?}.count.to_s << " out of " << cow.count.to_s << "\n"
+
+      # g = Gruff::Scatter.new(800)
+      # g.title = bat.zip(val).join(", ")
+      # g.data(:control, [0, 10], [0, 10])
+      # live_xs = live.map{|x| x.send(:gimme, bat)[0].to_i}
+      # puts live_xs.inspect
+      # live_ys = live.map{|x| x.send(:gimme, bat)[1].to_i}
+      # g.data(:live, live_xs, live_ys)
+      # dead_xs = dead.map{|x| x.send(:gimme, bat)[0].to_i}
+      # dead_ys = dead.map{|x| x.send(:gimme, bat)[1].to_i}
+      # g.data(:dead, dead_xs, dead_ys)
+      # g.write('pngs/' + g.title + ".png")
+      percentage = (live.count / cow.count.to_f).round(3)
+
+      if percentage > 0.7 || percentage < 0.3
+        puts "\t\t" << cow.count.to_s <<  " people had #{val} for #{bat}"
+        puts "\t\t" << percentage.to_s << " of those survived"
+        puts "\t\t" << live.count.to_s << " out of " << cow.count.to_s << "\n"
+      end
     end
   end
 
